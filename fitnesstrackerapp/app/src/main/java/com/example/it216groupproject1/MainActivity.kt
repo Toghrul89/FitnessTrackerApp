c21f4fb2feb1da8649f238e435a3eda3c6164723
Toghrul89 <116531144+Toghrul89@users.noreply.github.com>
2024-06-12 19:04:23 -0700

    Update MainActivity.kt

--- fitnesstrackerapp/app/src/main/java/com/example/it216groupproject1/MainActivity.kt
+++ fitnesstrackerapp/app/src/main/java/com/example/it216groupproject1/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.example.it216groupproject1
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var enterName: EditText
+    private lateinit var setGoalButton: Button
+    private lateinit var trackActivitiesButton: Button
+    private lateinit var viewProgressButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        enterName = findViewById(R.id.enterName)
+        setGoalButton = findViewById(R.id.setGoalButton)
+        trackActivitiesButton = findViewById(R.id.trackActivitiesButton)
+        viewProgressButton = findViewById(R.id.viewProgressButton)
+
+        setGoalButton.setOnClickListener {
+            val intent = Intent(this, SetGoalActivity::class.java)
+            startActivity(intent)
+        }
+
+        trackActivitiesButton.setOnClickListener {
+            val intent = Intent(this, TrackActivitiesActivity::class.java)
+            startActivity(intent)
+        }
+
+        viewProgressButton.setOnClickListener {
+            val intent = Intent(this, ViewProgressActivity::class.java)
+            startActivity(intent)
         }
     }
 }
